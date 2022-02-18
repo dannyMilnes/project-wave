60182920a7c32a947a573405715e766c6a7a4f96
Daniel Milnes <daniel.milnes@deeperthanblue.co.uk>
2022-02-18 16:46:12 +0000

    low power mode test 10

--- script.js
+++ script.js
@@ -1,34 +1,18 @@
-Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
-    get: function () {
-        return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
-    }
+$(document).ready(function () {
+    Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+        get: function () {
+            return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+        }
+    });
+    $('body').on('click touchstart', function () {
+        const videoElement = document.getElementById('liquidBackground');
+        if (videoElement.playing) {
+            // video is already playing so do nothing
+        }
+        else {
+            // video is not playing
+            // so play video now
+            videoElement.play();
+        }
+    });
 });
-
-$('body').on('click touchstart', function () {
-    const videoElement = document.getElementById('liquidBackground');
-    if (videoElement.playing) {
-        // video is already playing so do nothing
-    }
-    else {
-        // video is not playing
-        // so play video now
-        videoElement.play();
-    }
-});
-
-// Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
-//     get: function () {
-//         return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
-//     }
-// });
-
-// const video = document.getElementById('liquidBackground');
-// video.addEventListener('suspend', () => {
-//     // We're in low-power mode, show fallback UI
-// });
-// video.addEventListener('play', () => {
-//     // Remove fallback UI if user plays video manually
-// });
-
-
-Object.defineProperty(HTMLMediaElement.prototype, 'playing', { get: function () { return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2); } }); jQuery('body').on('click touchstart', function () { const videoElement = document.getElementById('liquidBackground'); if (videoElement.playing) { // video is already playing so do nothing } else { // video is not playing // so play video now videoElement.play(); } });
