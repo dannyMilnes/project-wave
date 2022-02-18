46f72775a70389a22a1d632c035fda38c7e178bb
Daniel Milnes <daniel.milnes@deeperthanblue.co.uk>
2022-02-18 00:13:11 +0000

    low power mode test 2

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const video = document.getElementById('liquidBackground');
-video.addEventListener('suspend', () => {
-    // We're in low-power mode, show fallback UI
+$('body').on('click touchstart', function () {
+    const videoElement = document.getElementById('home_video');
+    if (videoElement.playing) {
+        // video is already playing so do nothing
+    }
+    else {
+        // video is not playing
+        // so play video now
+        videoElement.play();
+    }
 });
-video.addEventListener('play', () => {
-    // Remove fallback UI if user plays video manually
-});
+
+// Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+//     get: function () {
+//         return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+//     }
+// });
+
+// const video = document.getElementById('liquidBackground');
+// video.addEventListener('suspend', () => {
+//     // We're in low-power mode, show fallback UI
+// });
+// video.addEventListener('play', () => {
+//     // Remove fallback UI if user plays video manually
+// });
