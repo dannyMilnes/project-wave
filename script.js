95093a5479b520798e96e594a49099ac7032f204
Daniel Milnes <daniel.milnes@deeperthanblue.co.uk>
2022-02-20 15:15:00 +0000

    low power mode bavkground fix 12

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-$(document).ready(function () {
-    Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
-        get: function () {
-            return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
-        }
-    });
-    $('body').on('click touchstart', function () {
-        const videoElement = document.getElementById('liquidBackground');
-        if (videoElement.playing) {
-            // video is already playing so do nothing
-        }
-        else {
-            // video is not playing
-            // so play video now
-            videoElement.play();
-        }
-    });
-});
+// $(document).ready(function () {
+//     Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+//         get: function () {
+//             return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+//         }
+//     });
+//     $('body').on('click touchstart', function () {
+//         const videoElement = document.getElementById('liquidBackground');
+//         if (videoElement.playing) {
+//             // video is already playing so do nothing
+//         }
+//         else {
+//             // video is not playing
+//             // so play video now
+//             videoElement.play();
+//         }
+//     });
+// });
 
-$(document).ready(function () {
-    setTimeout(function () {
-        $('#click-down').trigger('click');
-        console.log('Button Down Clicked');
-    }, 2000);
-    $('liquidBackground').controls = false;
-});
+// $(document).ready(function () {
+//     setTimeout(function () {
+//         $('#click-down').trigger('click');
+//         console.log('Button Down Clicked');
+//     }, 2000);
+//     $('liquidBackground').controls = false;
+// });
